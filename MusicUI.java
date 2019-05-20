4e2c4c64470cfa3e1db827bf0ccdad2fd844e58e
SmitPurohit <35670650+SmitPurohit@users.noreply.github.com>
2019-05-20 17:23:51 -0500

    Add files via upload
    Adds the files
    Currently plays .mp3 files in the same directory
    The skip button only works once for some reason

---
+++ MusicUI.java
@@ -0,0 +1,56 @@
+//This class will serve as the main "runner/player" class of the MusicPlayer
+//The code here will play the music, and be the user interaction class
+import java.awt.event.*;  
+import javax.swing.*;    
+public class MusicUI implements ItemListener
+{
+   boolean playing; //is music playing
+   
+   static Player player;
+   static JToggleButton button; 
+   public static void main(String args[])
+   {
+      
+      
+      
+      button = new JToggleButton("ON");  
+      button.addItemListener(new ItemListener() {
+   public void itemStateChanged(ItemEvent ev) {
+      if(ev.getStateChange()==ItemEvent.SELECTED){
+        player.nextSong();
+      } else if(ev.getStateChange()==ItemEvent.DESELECTED){
+        
+      }
+   }
+});
+      //setAction();  
+      JFrame f=new JFrame("Button Example");  
+    /*JButton b=new JButton("Skip");  
+    b.setBounds(50,100,95,30);  
+     b.addActionListener(new ActionListener(){  
+   public void actionPerformed(ActionEvent e){  
+            player.nextSong();
+        }  
+    });  */
+    f.add(button);
+    f.setSize(400,400);  
+    //f.setLayout(null);  
+    f.setVisible(true);
+      player = new Player(button);
+      while(true)
+      {
+         player.playSong();
+      }
+   }
+    
+  
+    
+     
+    public void itemStateChanged(ItemEvent eve) {  
+        if (button.isSelected())  
+            button.setText("OFF");  
+        else  
+            button.setText("ON");  
+    }  
+    }
+   
---
+++ Player.java
@@ -0,0 +1,175 @@
+//This class is the player of the music ... duh
+
+import java.util.ArrayList;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.*;
+import javafx.*;
+import javafx.embed.swing.JFXPanel;
+import org.jaudiotagger.audio.*;
+import java.util.Date;
+
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class Player extends JPanel implements KeyListener, ActionListener, FocusListener
+{
+   public String nameSong;
+   public ArrayList<String> songList = new ArrayList<String>();
+   public boolean skip = false;
+   private static Media hit;
+   private static MediaPlayer mediaPlayer;
+   int songIndex;
+   public boolean shuffle = false; //shuffle the songs?
+   JToggleButton button;
+   
+   public Player(JToggleButton button)
+   {
+      this.button = button;
+      button.addItemListener(new ItemListener() {
+   public void itemStateChanged(ItemEvent ev) {
+      if(ev.getStateChange()==ItemEvent.SELECTED){
+        skip = true;
+      } else if(ev.getStateChange()==ItemEvent.DESELECTED){
+        
+      }
+   }
+});
+      addKeyListener(this);
+       setFocusable(true);
+      setFocusTraversalKeysEnabled(false);
+      songIndex = 0;
+      JFXPanel pp = new JFXPanel();
+      File dir = new File(System.getProperty("user.dir"));
+      
+      //only opens .mo3 files
+      File[] filesInDir = dir.listFiles(
+         new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+               return name.toLowerCase().endsWith(".mp3");
+            }});
+   
+      //fills in the songlist with the files in the same directory as this file if the extension is .mp3
+   
+      for(File file: filesInDir)
+      {
+         String fileName = file.getName();
+      
+         songList.add(fileName);
+         System.out.println(fileName);
+      }
+      
+      
+      
+   
+   }
+   
+   public void playSong()
+   {
+      //if there is no shuffle
+      int current = (int)(System.currentTimeMillis()/1000);
+      if(!shuffle)
+      {
+         String song = songList.get(songIndex);
+         System.out.println(song);
+         hit = new Media(new File(song).toURI().toString());
+         mediaPlayer = new MediaPlayer(hit);
+         mediaPlayer.play();
+         while(songGoingOn(current, song))//doesnt do anything until the song is done
+         {
+            if(skip)
+            {
+               skip = false;
+               break;
+               }
+         }
+         nextSong();
+      
+      }
+      //if shuffle is on
+      else{
+         songIndex = (int)(Math.random()*songList.size());
+         String song = songList.get(songIndex);
+         hit = new Media(new File(song).toURI().toString());
+         mediaPlayer = new MediaPlayer(hit);
+         mediaPlayer.play();
+         while(songGoingOn(current, song))//doesnt do anything until the song is done
+         {
+            if(skip)
+               break;
+         }
+         
+      }
+   }
+
+   public boolean songGoingOn(int current, String song)
+   {
+      int duration = 3;
+   
+      try {
+         AudioFile audioFile = AudioFileIO.read(new File(song));
+         duration += audioFile.getAudioHeader().getTrackLength();
+      
+      } catch (Exception e) {
+         e.printStackTrace();
+      
+      }
+      int newCurrent = (int)(System.currentTimeMillis()/1000);
+      //skip = newCurrent-current == 10;
+      
+      
+      System.out.println(newCurrent-current);
+      
+      return (newCurrent-current)<=duration;
+   }
+   public void nextSong()
+   {
+      mediaPlayer.stop();
+         songIndex++;
+         
+         if(songIndex>=songList.size())
+            songIndex = 0;
+      this.playSong();
+   }
+   
+   @Override
+   public void actionPerformed(ActionEvent e){  
+            this.nextSong();
+        } 
+   
+   @Override
+   public void keyTyped(KeyEvent e){
+    int keyCode = e.getKeyCode();
+      if(keyCode == KeyEvent.VK_LEFT)
+         {
+       
+        skip = true;
+        System.out.println(skip);
+    }}
+    
+    @Override
+    public void keyPressed(KeyEvent e){
+    if(e.getKeyCode() == KeyEvent.VK_S)
+       {
+        skip = true;
+        System.out.println(skip);
+        mediaPlayer.stop();
+         songIndex++;
+    }
+    }
+    
+    @Override
+    public void keyReleased(KeyEvent e){
+    if(e.getKeyCode() == KeyEvent.VK_S)
+       {
+        skip = false;
+        System.out.println("Skip: " + skip);
+    }}
+    @Override
+   public void focusGained(FocusEvent e){}
+   
+   @Override
+   public void focusLost(FocusEvent e){}
+   
+}
